aeeed0cac630589929bb411e3c4526cf52e4135d
mdxaasil <mdaasil18@gmail.com>
2023-05-29 23:16:42 +0530

    final cors changes

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app=express();
 const socketio= require('socket.io');
 const mongoose = require('mongoose');
 const expressServer= app.listen(3001);
-const io = socketio(expressServer);
+const cors = require('cors');
+
+// const io = socketio(expressServer);
+const io=socketio(expressServer,{
+    cors:{
+        origin:'*'
+    }
+});
+
 
 const Game=require('./models/Game');
 const QuotableAPI =require('./QuotableAPI');
